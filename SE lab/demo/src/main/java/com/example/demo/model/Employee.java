443e595339dd604b48430b8977f7208a952ea724
Manhaas10 <159009853+Manhaas10@users.noreply.github.com>
2025-01-01 15:05:22 +0530

    demo project on springboot

---
+++ SE lab/demo/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String Name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
